e3447eb9a13cfdfb7fe981ce2696dc8b6b1a5bac
Ryan Kenney <ryan@ryankenney.info>
2016-05-21 13:29:31 -0700

    demo-pure-markdown * Simplified link processing in bootstrap.js with logic from other demo demo-local-filesystem * Cleaned up bootstrap.js * Unified link processing in bootstrap.js with logic from other demo

--- demo-local-filesystem/js/bootstrap.js
+++ demo-local-filesystem/js/bootstrap.js
@@ -1,50 +1,7 @@
-
-var head = document.getElementsByTagName('head')[0]
-var link = document.createElement("link")
-link.setAttribute("rel", "stylesheet")
-link.setAttribute("type", "text/css")
-link.setAttribute("href", "css/theme.css")
-head.appendChild(link);
-
-var body = document.getElementsByTagName('body')[0]
-var link = document.createElement("link")
-
-var newScript = document.createElement('script');
-newScript.type = 'text/javascript';
-newScript.src = 'js/jquery-1.12.3.js';
-document.getElementsByTagName('head')[0].appendChild(newScript);
-newScript = document.createElement('script');
-newScript.type = 'text/javascript';
-newScript.src = 'js/markdown.js';
-document.getElementsByTagName('head')[0].appendChild(newScript);
-newScript = document.createElement('script');
-newScript.type = 'text/javascript';
-newScript.src = 'js/purl.js';
-document.getElementsByTagName('head')[0].appendChild(newScript);
-
-
-function updateMarkdownLinks(markdownJson) {
-    var refs = markdownJson[1].references;
-    updateMarkdownLinks_Inner(markdownJson, refs);
-}
-
-function updateMarkdownLinks_Inner(markdownJson, refs) {
-    for (var item = 0; item < markdownJson.length; item++) {
-        if ( markdownJson[item] === "link_ref" ) {
-            var ref = markdownJson[1].ref;
-            // if there's no reference, define a wiki link
-            var url = refs[ref];
-            if (!url || !url.href || !(/^[\w\- ]+\.md$/.exec(url.href))) {
-                continue;
-            }
-            refs[ref] = {
-                href: "my.html?md=" + url.href.replace(/\s/, "+")
-            };
-        } else if ( Array.isArray(markdownJson[item])) {
-            markdownJson[item].forEach(updateMarkdownLinks_Inner);
-        }
-    }
-}
+loadCss('css/theme.css');
+loadJs('js/jquery-1.12.3.js');
+loadJs('js/markdown.js');
+loadJs('js/purl.js');
 
 window.onload = function(){
     var tree = markdown.parse($("#markdown").text());
@@ -52,3 +9,34 @@ window.onload = function(){
     var html = markdown.toHTML(tree);
     $("#markdown").html(html);
 };
+
+function updateMarkdownLinks(markdownJson) {
+    if ( markdownJson[0] === "link" ) {
+        var url = markdownJson[1];
+        if (!url || !url.href || !(/^[\w\- ]+\.md$/.exec(url.href))) {
+            return;
+        }
+        markdownJson[1].href = "index.html?md=" + encodeURIComponent(url.href);
+    } else {
+        for (var item = 1; item < markdownJson.length; item++) {
+            if (Array.isArray(markdownJson[item])) {
+                markdownJson[item].forEach(updateMarkdownLinks);
+            }
+        }
+    }
+}
+
+function loadJs(scriptFile) {
+    var newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    newScript.src = scriptFile;
+    document.getElementsByTagName('head')[0].appendChild(newScript);
+}
+
+function loadCss(cssFile) {
+    var link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("type", "text/css");
+    link.setAttribute("href", cssFile);
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
